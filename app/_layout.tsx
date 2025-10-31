fa15c527c073d129decca7db3fc44e8936d95f63
arturo0427 <arturo_munoz27@outlook.com>
2025-10-31 10:35:52 -0500

    tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="about" options={{ title: "About" }} />
     </Tabs>
   );
 }
